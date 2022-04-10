06901177175ceef272fd7f5d2533766570c07aab
Yorick Fredrix <yorick.fredrix@outlook.com>
2022-04-10 23:42:24 +0200

    Fixed unit test

--- tests/unit/test_dataobjects.py
+++ tests/unit/test_dataobjects.py
@@ -1,10 +1,10 @@
-from nsdepaturetimes.datacollection import Depatures
+from nsdepaturetimes.datacollection import Departures
 from datetime import datetime, timezone, timedelta
 
 
 def test_parse_depatures():
-    depature = Depatures()
-    depature.parse_depatures(
+    departure = Departures()
+    departure.parse_departures(
         {
             "payload": {
                 "departures": [
@@ -22,12 +22,12 @@ def test_parse_depatures():
             }
         }
     )
-    assert len(depature.depatures) == 1
-    assert depature.depatures[1234]["name"] == "Amsterdam Centraal"
-    assert depature.depatures[1234]["actualDateTime"] == datetime(2019,12,4,12,0,0,tzinfo=timezone(timedelta(hours=1)))
-    assert depature.depatures[1234]["plannedDateTime"] == datetime(2019,12,4,12,0,0,tzinfo=timezone(timedelta(hours=1)))
-    assert depature.depatures[1234]["cancelled"] == False
-    assert depature.depatures[1234]["direction"] == "Oost"
-    assert depature.depatures[1234]["track"] == "1"
-    assert depature.depatures[1234]["trainCategory"] == "Intercity"
-    assert depature.depatures[1234]["trainNumber"] == 1234
+    assert len(departure.departures) == 1
+    assert departure.departures[1234]["name"] == "Amsterdam Centraal"
+    assert departure.departures[1234]["actualDateTime"] == datetime(2019,12,4,12,0,0,tzinfo=timezone(timedelta(hours=1)))
+    assert departure.departures[1234]["plannedDateTime"] == datetime(2019,12,4,12,0,0,tzinfo=timezone(timedelta(hours=1)))
+    assert departure.departures[1234]["cancelled"] == False
+    assert departure.departures[1234]["direction"] == "Oost"
+    assert departure.departures[1234]["track"] == "1"
+    assert departure.departures[1234]["trainCategory"] == "Intercity"
+    assert departure.departures[1234]["trainNumber"] == 1234
