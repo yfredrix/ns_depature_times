632838e1b9ea7df0d1d21835263afa8373fe7e65
Yorick Fredrix <yorick.fredrix@outlook.com>
2022-04-24 14:34:32 +0200

    Added support for minimal departure time and updated layout with remark or route

--- nsdeparturetimes/generate_pages/generate_displays.py
+++ nsdeparturetimes/generate_pages/generate_displays.py
@@ -1,12 +1,11 @@
 import os
-from datetime import datetime
 
-import pytz
 from app import app
 from dash import html
 from datacollection import ApiConnections, Departures
 
 STATION_CODE = os.getenv("STATION_CODE")
+MINIMUM_DEPATURE_TIME = int(os.getenv("MINIMUM_DEPARTURE_TIME", "0"))
 connection = ApiConnections()
 
 station_info = connection.get_station(station_code=STATION_CODE)
@@ -23,22 +22,6 @@ def generate_layout() -> list:
     trains = connection.get_trains(departures.departures.keys())
     departures.update_departures(trains)
 
-    header = html.Div(
-        children=[
-            html.Div(
-                className="clock",
-                children=[
-                    html.Img(src=app.get_asset_url("clock.svg"), className="clockimg"),
-                    datetime.now(tz=pytz.timezone("Europe/Amsterdam")).strftime(
-                        "%H:%M"
-                    ),
-                    html.Div(children=[departures.stationName], className="station"),
-                ],
-            )
-        ],
-        id="departures-header",
-        className="style-tb",
-    )
     new_table_layout = html.Div(
         id="departures-main",
         className="style-tb",
@@ -58,134 +41,136 @@ def generate_layout() -> list:
             *generate_html_departure_display(departures.departures),
         ],
     )
-    return [header, new_table_layout]
+    return new_table_layout
 
 
 def generate_html_departure_display(departures: dict) -> list:
     new_layout = []
     for train, data in departures.items():
-        if len(data["materieel"]) > 0:
-            total_width = 0
-            for materieel in data["materieel"]:
-                total_width += materieel["breedte"]
-        if data["cancelled"]:
-            row_class = "row cancelled"
-        else:
-            row_class = "row"
-        new_layout += [
-            html.Div(
-                className=row_class,
-                children=[
-                    html.Div(
-                        className="column time",
-                        children=[
-                            html.Div(
-                                className="departures-time departure_time",
-                                children=[
-                                    html.Div(
-                                        className="departure_time",
-                                        children=data["actualDateTime"].strftime(
-                                            "%H:%M"
-                                        ),
-                                    )
-                                ],
-                            )
-                        ],
-                    ),
-                    html.Div(
-                        className="column destination",
-                        children=[
-                            html.Div(
-                                className="destinations",
-                                children=html.Div(
-                                    className="actual-destination",
-                                    children=data["direction"],
-                                ),
-                            ),
-                            html.Div(
-                                id={
-                                    "trainNumber": train,
-                                    "type": "switching",
-                                    "category": "via",
-                                }
-                                if data["messages"]
-                                else {
-                                    "trainNumber": train,
-                                    "type": "normal",
-                                    "category": "via",
-                                },
-                                className="via",
-                                children=f"via {','.join(data['via'])}"
-                                if len(data["via"]) > 0
-                                else "",
-                            ),
-                            html.Div(
-                                id={
-                                    "trainNumber": train,
-                                    "type": "switching",
-                                    "category": "remark",
-                                },
-                                className="remarks",
-                                children=data["messages"],
-                            )
-                            if data["messages"]
-                            else "",
-                        ],
-                    ),
-                    html.Div(
-                        className="column platform",
-                        children=[
-                            html.Div(
-                                className="platform-indicator platform-changed"
-                                if data["changedPlatform"]
-                                else "platform-indicator",
-                                children=data["track"],
-                            )
-                        ],
-                    ),
-                    html.Div(
-                        className="column train",
-                        children=[
-                            html.Div(
-                                className="train-id",
-                                children=[
-                                    train,
-                                    html.Div(
-                                        className="crowdedness",
-                                        children=html.Img(
-                                            src=app.get_asset_url(
-                                                f'{data["classifiction"]}.svg'
-                                            )
-                                        ),
-                                    ),
-                                ],
-                            )
-                        ],
-                    ),
-                    html.Div(
-                        className="column material",
-                        children=[
-                            html.Span(
-                                [
-                                    html.Span(
-                                        html.Img(
-                                            src=i["afbeelding"],
-                                            style={
-                                                "maxWidth": f"{i['breedte']/total_width*100}%",
-                                                "maxHeight": "100%",
-                                            },
+        if data["timeBeforeLeave"] >= MINIMUM_DEPATURE_TIME:
+            if len(data["materieel"]) > 0 and "afbeelding" in data["materieel"][0].keys():
+                total_width = 0
+                for materieel in data["materieel"]:
+                    total_width += materieel["breedte"]
+            if data["cancelled"]:
+                row_class = "row cancelled"
+            else:
+                row_class = "row"
+            new_layout += [
+                html.Div(
+                    className=row_class,
+                    children=[
+                        html.Div(
+                            className="column time",
+                            children=[
+                                html.Div(
+                                    className="departures-time departure_time",
+                                    children=[
+                                        html.Div(
+                                            className="departure_time",
+                                            children=data["actualDateTime"].strftime(
+                                                "%H:%M"
+                                            ),
                                         )
-                                    )
-                                    for i in data["materieel"]
-                                    if "afbeelding" in i.keys()
-                                ]
-                            ),
-                        ],
-                    )
-                    if len(data["materieel"]) > 0
-                    else "",
-                ],
-            )
-        ]
+                                    ],
+                                )
+                            ],
+                        ),
+                        html.Div(
+                            className="column destination",
+                            children=[
+                                html.Div(
+                                    className="destinations",
+                                    children=html.Div(
+                                        className="actual-destination",
+                                        children=data["direction"],
+                                    ),
+                                ),
+                                html.Div(
+                                    id={
+                                        "trainNumber": train,
+                                        "type": "switching",
+                                        "category": "via",
+                                    }
+                                    if data["messages"]
+                                    else {
+                                        "trainNumber": train,
+                                        "type": "normal",
+                                        "category": "via",
+                                    },
+                                    className="via",
+                                    children=f"via {', '.join(data['via'])}"
+                                    if len(data["via"]) > 0
+                                    else "",
+                                ),
+                                html.Div(
+                                    id={
+                                        "trainNumber": train,
+                                        "type": "switching",
+                                        "category": "remark",
+                                    },
+                                    className="remarks",
+                                    children=data["messages"],
+                                )
+                                if data["messages"]
+                                else "",
+                            ],
+                        ),
+                        html.Div(
+                            className="column platform",
+                            children=[
+                                html.Div(
+                                    className="platform-indicator platform-changed"
+                                    if data["changedPlatform"]
+                                    else "platform-indicator",
+                                    children=data["track"],
+                                )
+                            ],
+                        ),
+                        html.Div(
+                            className="column train",
+                            children=[
+                                html.Div(
+                                    className="train-id",
+                                    children=[
+                                        f"{data['trainCategory']} {train}",
+                                        html.Div(
+                                            className="crowdedness",
+                                            children=html.Img(
+                                                src=app.get_asset_url(
+                                                    f'{data["classifiction"]}.svg'
+                                                )
+                                            ),
+                                        ),
+                                    ],
+                                )
+                            ],
+                        ),
+                        html.Div(
+                            className="column material",
+                            children=[
+                                html.Span(
+                                    [
+                                        html.Span(
+                                            html.Img(
+                                                src=i["afbeelding"],
+                                                style={
+                                                    "maxWidth": f"{i['breedte']/total_width*100}%",
+                                                    "maxHeight": "100%",
+                                                },
+                                            )
+                                        )
+                                        for i in data["materieel"]
+                                        if "afbeelding" in i.keys()
+                                    ]
+                                ),
+                            ],
+                        )
+                        if len(data["materieel"]) > 0
+                        and "afbeelding" in data["materieel"][0].keys()
+                        else "",
+                    ],
+                )
+            ]
 
     return new_layout
--- nsdeparturetimes/main.py
+++ nsdeparturetimes/main.py
@@ -1,26 +1,69 @@
-import os
-from dash import html, dcc, Input, Output, State
+from datetime import datetime
+
+import pytz
+from dash import Input, Output, State, dcc, html, ALL, callback_context
 
 from app import app, server
+from generate_pages import departures, generate_layout
 
-from generate_pages import generate_layout
-
-
-STATION_CODE = os.getenv("STATION_CODE")
 
 app.layout = html.Div(
     children=[
         dcc.Interval(id="data_refresh", interval=30000, n_intervals=0),
+        dcc.Interval(id="style_updates", interval=5000, n_intervals=0),
+        html.Div(
+            children=[
+                html.Div(
+                    className="clock",
+                    children=[
+                        html.Img(
+                            src=app.get_asset_url("clock.svg"), className="clockimg"
+                        ),
+                        html.Div(
+                            datetime.now(tz=pytz.timezone("Europe/Amsterdam")).strftime(
+                                "%H:%M"
+                            ),
+                            id="clocktime",
+                            className="clock",
+                        ),
+                        html.Div(
+                            children=[departures.stationName], className="station"
+                        ),
+                    ],
+                )
+            ],
+            id="departures-header",
+            className="style-tb",
+        ),
         html.Div(id="page_contents", children=generate_layout()),
     ]
 )
 
+
 @app.callback(
     Output("page_contents", "children"),
+    Output("clocktime", "children"),
     Input("data_refresh", "n_intervals"),
 )
 def update_layout(n_intervals: int) -> list:
-    return generate_layout()
+    return generate_layout(), datetime.now(
+        tz=pytz.timezone("Europe/Amsterdam")
+    ).strftime("%H:%M")
+
+
+@app.callback(
+    Output({"category": "via", "trainNumber": ALL, "type": "switching"}, "hidden"),
+    Output({"category": "remark", "trainNumber": ALL, "type": "switching"}, "hidden"),
+    Input("style_updates", "n_intervals"),
+)
+def switch_style(n_intervals):
+    given_outputs = callback_context.outputs_list
+    if len(given_outputs[0]) == len(given_outputs[1]):
+        if n_intervals % 2 == 0:
+            return len(given_outputs[0]) * [False], len(given_outputs[1]) * [True]
+        else:
+            return len(given_outputs[0]) * [True], len(given_outputs[1]) * [False]
+
 
 if __name__ == "__main__":
     app.run_server(host="192.168.1.5", debug=True)
